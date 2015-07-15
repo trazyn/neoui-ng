bb38ff56a3299ba3259d8c3b869f4fb975f5a464
trazyn <tn.razy@gmail.com>
2015-07-15 17:46:56 +0800

    update toast

--- src/components/toast/toast.js
+++ src/components/toast/toast.js
@@ -3,7 +3,7 @@ define( [ "ui/modal/modal" ], function() {
 
     "use strict";
 
-    function show( message, position ) {
+    function show( message, position, class4toast, delay ) {
 
         var modal = $.modal( {
             showHead        : false,
@@ -12,16 +12,18 @@ define( [ "ui/modal/modal" ], function() {
             closeByDocument : false,
             fadeIn          : false,
             animate         : "scale",
-            class4modal     : "ui toast " + position,
+            class4modal     : "ui toast " + [ position, class4toast || "" ].join( " " ),
 
             render          : function( ready, loading, close ) {
 
                 var template = "<p class='message'>" + message + "</p>" +
-                                "<i class='icon close'></i>";
+                                "<i class='icon close transition rotate'></i>";
 
                 this
                 .html( template )
                 .delegate( "i.icon.close", "click", close );
+
+                setTimeout( close, delay || 3000 );
             }
         } );
 
@@ -30,20 +32,32 @@ define( [ "ui/modal/modal" ], function() {
 
     $.toast = {
 
-        top: function( message ) {
-            show( message, "top" );
+        top: function( message, delay, class4toast ) {
+
+            return {
+
+                left: function() {
+                    show( message, "topleft", class4toast, delay );
+                },
+
+                right: function() {
+                    show( message, "topright", class4toast, delay );
+                }
+            };
         },
 
-        bottom: function() {
-            show( message, "bottom" );
-        },
+        bottom: function( message, delay, class4toast ) {
 
-        left: function() {
-            show( message, "left" );
-        },
+            return {
 
-        right: function() {
-            show( message, "right" );
+                left: function() {
+                    show( message, "bottomleft", class4toast, delay );
+                },
+
+                right: function() {
+                    show( message, "bottomright", class4toast, delay );
+                }
+            };
         }
     };
 } );
