85799ed705daaa32219042e2ae830c05ea70092f
trazyn <tn.razy@gmail.com>
2015-09-10 21:02:22 +0800

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,28 +10,16 @@ browserSync = require( "browser-sync" ),
 less = require( "gulp-less" ),
 debug = require( "gulp-debug" ),
 streamqueue = require( "streamqueue" ),
+clean = require( "gulp-clean" ),
+rev = require( "gulp-rev" ),
+rjs = require( "requirejs" ),
+amdclean = require( "amdclean" ),
 
 fs = require( "fs" ),
 pkg = require( "./package.json" ),
 
 /** Task definitions */
 bs, gulp = require( "gulp" )
-
-	.task( "vendor", function() {
-
-		return gulp.src( [ "bower_components/jquery/dist/jquery.js",
-		        "bower_components/jquery.event.drag-new/event.drag/jquery.event.drag.js",
-				"bower_components/angular/angular.js",
-				"bower_components/angular-route/angular-route.js",
-				"bower_components/requirejs/require.js" ] )
-
-			.pipe( debug() )
-            .pipe( concat( "vendor.js" ) )
-			.pipe( gulp.dest( pkg.dest ) )
-			.pipe( uglify() )
-			.pipe( rename( "vendor.min.js" ) )
-			.pipe( gulp.dest( pkg.dest ) );
-	} )
 
 	.task( "start", function() {
 
@@ -44,7 +32,7 @@ bs, gulp = require( "gulp" )
 		} );
 	} )
 
-	.task( "css", function() {
+	.task( "dist", function() {
 
 		var
 		dest = pkg.dest,
@@ -52,7 +40,9 @@ bs, gulp = require( "gulp" )
         cleancss = new (require( "less-plugin-clean-css" ))( { advanced: true, compatibility: "ie8" } ),
         autoprefix = new (require( "less-plugin-autoprefix" ))( { browsers: [ "last 4 versions" ] } );
 
-        return streamqueue( { objectMode: true },
+        gulp.src( "dist", { read: false } ).pipe( clean() );
+
+        streamqueue( { objectMode: true },
                 gulp.src( "src/style/main.less" ),
                 gulp.src( [ "src/components/**/*.less", "!src/components/**/*-bs.less"] ),
                 gulp.src( "src/demo/**/*.less" ) )
@@ -62,11 +52,64 @@ bs, gulp = require( "gulp" )
 			.pipe( gulp.dest( dest ) )
 			.pipe( minifyCSS() )
 			.pipe( rename( "css.min.css" ) )
+			.pipe( gulp.dest( dest ) )
+			.pipe( rev() )
 			.pipe( gulp.dest( dest ) );
+
+		gulp.src( [ "bower_components/jquery/dist/jquery.js",
+		        "bower_components/jquery.event.drag-new/event.drag/jquery.event.drag.js",
+				"bower_components/angular/angular.js",
+				"bower_components/angular-route/angular-route.js",
+				"bower_components/requirejs/require.js" ] )
+
+			.pipe( debug() )
+            .pipe( concat( "vendor.js" ) )
+			.pipe( gulp.dest( dest ) )
+			.pipe( uglify() )
+			.pipe( rename( "vendor.min.js" ) )
+			.pipe( gulp.dest( dest ) );
+
+		rjs.optimize( {
+		    baseUrl: "src",
+            include: "bootstrap",
+            out: "dist/app.js",
+            paths: {
+                "ui": "components",
+                "ui/accordion/accordion-ng": "empty:",
+                "ui/autoComplete/autoComplete-ng": "empty:",
+                "ui/calendar/calendar-ng": "empty:",
+                "ui/dropdown/dropdown-ng": "empty:",
+                "ui/loading/loading-ng": "empty:",
+                "ui/message/message-ng": "empty:",
+                "ui/modal/modal-ng": "empty:",
+                "ui/pagination/pagination-ng": "empty:",
+                "ui/progress/progress-ng": "empty:",
+                "ui/rate/rate-ng": "empty:",
+                "ui/ripple/ripple-ng": "empty:",
+                "ui/sidenav/sidenav-ng": "empty:",
+                "ui/tab/tab-ng": "empty:",
+                "ui/toast/toast-ng": "empty:",
+                "ui/tree/tree-ng": "empty:",
+                "ui/validation/validation-ng": "empty:",
+                "util/dateutil": "empty:"
+            }
+		} );
+
+        rjs.optimize( {
+            baseUrl: "src",
+            paths: {
+                ui: "components"
+            },
+            include: "bundle",
+            out: "dist/neoui-ng-rjs.js",
+            onModuleBundleComplete: function( data ) {
+                fs.writeFileSync( "dist/neoui-ng.js", amdclean.clean( { "filePath": data.path } ) );
+            }
+        } );
 	} )
 
 	.task( "watch", function() {
-		gulp.watch( "src/**/*.less", [ "css" ] );
+		gulp.watch( "src/**/*.less", [ "dist" ] );
 	} )
 
     /** Build the test data */
@@ -120,7 +163,6 @@ bs, gulp = require( "gulp" )
 		walk( "./", function( err, result ) {
 
 		    if ( err ) throw err;
-
             fs.writeFile( "./src/demo/tree/files.json", JSON.stringify( result ) );
 		} );
 	} )
@@ -132,9 +174,7 @@ bs, gulp = require( "gulp" )
 			.pipe( jshint.reporter( "default" ) );
 	} )
 
-	.task( "init", [ "vendor", "css" ] )
-
-	.task( "default", [ "data", "watch", "css", "start" ] );
+	.task( "default", [ "data", "watch", "dist", "start" ] );
 
 pkg.dest = pkg.dest || "dist";
 fs.existsSync( pkg.dest ) || fs.mkdirSync( pkg.dest );
